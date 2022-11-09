864e9a0f2e90d39deeb7590826cd1686f917ab27
afmireski <afmireski@outlook.com>
2022-11-09 16:12:11 -0300

    #2 - Add tests declarations in 'main.c'

--- list/main.c
+++ list/main.c
@@ -10,8 +10,33 @@
 
 #include <list.h>
 
+void verify(int condition, char *message);
+
+void test_list_create_and_destroy();
+void test_list_isEmpty();
+void test_list_append();
+void test_list_insert();
+void test_list_removeAt();
+void test_list_remove();
+void test_list_replace();
+void test_list_indexOf();
+void test_list_returnAt();
+void test_list_toString();
+
 int main()
 {
 
     return 0;
+}
+
+void verify(int condition, char *message)
+{
+    if (condition)
+    {
+        printf("[PASSED] : %s\n", message);
+    }
+    else
+    {
+        printf("[FAILED] : %s\n", message);
+    }
 }
