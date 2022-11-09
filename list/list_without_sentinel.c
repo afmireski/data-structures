f0d3a34cf5a46134a242ebfd90e7eddec248ca6c
afmireski <afmireski@outlook.com>
2022-11-09 14:13:59 -0300

    #2 - Implement 'create' in 'list_without_sentinel.c'

--- list/list_without_sentinel.c
+++ list/list_without_sentinel.c
@@ -29,20 +29,36 @@ struct list
 /***********************************************
 | ACTIONS
 ************************************************/
-List *list_create();
+List *list_create() {
+    List* l = (List*) malloc(sizeof(List));
+
+    l->begin = NULL;
+    l->end = NULL;
+    l->qty = 0;
+
+    return l;
+}
+
 void list_destroy(List **address);
+
 void list_print(List *l);
+
 int list_size(List *l);
+
 bool list_isEmpty(List *l);
 
 bool list_append(List *l, any element);
+
 bool list_insert(List *l, any element, int index);
 
 bool list_removeAt(List *l, int index, any *output);
+
 int list_remove(List *l, any element);
 
 bool list_replace(List *l, int index, any newElement);
+
 int list_indexOf(List *l, any element);
+
 bool list_returnAt(List *l, int index, any *output);
 
 bool list_toString(List *l, char *str);
