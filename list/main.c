c467eb08a4ec728b53bddd584d0eea7a43b9870d
afmireski <afmireski@outlook.com>
2022-11-09 16:23:08 -0300

    #3 - Implement 'create and destroy' test in 'main.c'

--- list/main.c
+++ list/main.c
@@ -8,7 +8,8 @@
 #include <string.h>
 #include <stdbool.h>
 
-#include <list.h>
+#include "list.h"
+
 
 void verify(int condition, char *message);
 
@@ -25,6 +26,29 @@ void test_list_toString();
 
 int main()
 {
+    printf("/***********************************************\n");
+    printf("LIST\n");
+    printf("/***********************************************\n\n");
+    // test_list_create_and_destroy();
+    printf("\n\n");
+    test_list_isEmpty();
+    printf("\n\n");
+    // test_list_append();
+    printf("\n\n");
+    // test_list_insert();
+    printf("\n\n");
+    // test_list_removeAt();
+    printf("\n\n");
+    // test_list_remove();
+    printf("\n\n");
+    // test_list_replace();
+    printf("\n\n");
+    // test_list_indexOf();
+    printf("\n\n");
+    // test_list_returnAt();
+    printf("\n\n");
+    // test_list_toString();
+    printf("/****************** END ********************/\n");
 
     return 0;
 }
@@ -39,4 +63,20 @@ void verify(int condition, char *message)
     {
         printf("[FAILED] : %s\n", message);
     }
+}
+
+
+
+void test_list_create_and_destroy() {
+    printf("/***********************************************\n");
+    printf("TEST: CREATE AND DESTROY\n");
+    printf("/***********************************************\n\n");
+
+    List* l = list_create();
+
+    verify(l != NULL, "Should create a new List");
+
+    list_destroy(&l);
+
+    verify(l == NULL, "Should destroy the List");
 }
